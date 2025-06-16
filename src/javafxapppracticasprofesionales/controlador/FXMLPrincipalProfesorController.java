ecba72663c87ba350baaa774cd417b22a23aed22
josxsg <grillo.silgom@gmail.com>
2025-06-16 01:49:05 -0600

    Vista principal profesor

---
+++ src/javafxapppracticasprofesionales/controlador/FXMLPrincipalProfesorController.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package javafxapppracticasprofesionales.controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author grill
+ */
+public class FXMLPrincipalProfesorController implements Initializable {
+
+    @FXML
+    private Label lbNombreVentana;
+    @FXML
+    private Label lbNombreUsuario;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void btnClicConsultarExpediente(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnClicValidarEntregaDocumentos(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnClicCerrarSesion(ActionEvent event) {
+    }
+    
+}
